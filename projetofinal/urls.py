3b7a318541aae4eff7015314bdf6f3c202c0136d
thais <thaiscorrea.s@outlook.com>
2016-03-29 22:34:41 -0300

    Mudanças do login

--- projetofinal/urls.py
+++ projetofinal/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url, patterns
 from . import views
 from .forms import Cadastro1Form, Cadastro2Form, Cadastro3Form, Cadastro4Form, Cadastro5Form, Cadastro6Form, Cadastro7Form, Cadastro8Form
-from .views import CadastroRealizado,CadastroWizard
+from .views import CadastroRealizado,CadastroWizard, EditarCadastro
 
 urlpatterns = [
     url(r'^$', views.home),
@@ -9,5 +9,6 @@ urlpatterns = [
     url(r'^cadastro$', CadastroWizard.as_view([Cadastro1Form,Cadastro2Form,Cadastro3Form,Cadastro4Form,Cadastro5Form,Cadastro6Form,Cadastro7Form,Cadastro8Form]), name="cadastro"),
     url(r'^cadastrado$', CadastroRealizado.as_view(), name="cadastro_realizado"),
     url(r'^login$', 'django.contrib.auth.views.login', {'template_name':'projetofinal/login.html'}, name='login'),
+    url(r'^editar$', EditarCadastro.as_view([Cadastro1Form,Cadastro2Form,Cadastro3Form,Cadastro4Form,Cadastro5Form,Cadastro6Form,Cadastro7Form,Cadastro8Form]), name="editar"),
     url(r'^sair/$', 'django.contrib.auth.views.logout_then_login', name='logout')
 ]
--- projetofinal/views.py
+++ projetofinal/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, TemplateView, FormView
-from django.core.urlresolvers import reverse_lazy
+from django.views.generic import TemplateView
 from .models import Paciente,User
+from .forms import Cadastro1Form
 from django.shortcuts import render_to_response
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseForbidden, HttpResponseRedirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.utils.translation import ugettext as _
 from formtools.wizard.views import SessionWizardView
 
 
 # Create your views here.
-
 def home(request):
     return render(request, 'projetofinal/home.html', {})
 
@@ -61,3 +67,65 @@ class CadastroWizard(SessionWizardView):
 
 class CadastroRealizado(TemplateView):
     template_name = "projetofinal/cadastrado.html"
+
+
+class EditarCadastro(SessionWizardView):
+    template_name = "projetofinal/editar.html"
+
+    def get_form_initial(self, step, **kwargs):
+        print(kwargs)
+        if 'id' in self.kwargs:
+            id = self.kwargs['id']
+            print(1)
+            project = Paciente.objects.get(id=id)
+            print (project)
+            from django.forms.models import model_to_dict
+            project_dict = model_to_dict(project)
+            return project_dict
+        else:
+            print(2)
+            return self.initial_dict.get(step, {})
+
+    def done(self, form_list, form_dict, **kwargs):
+        form_data= [form.cleaned_data for form in form_list]
+        user = User()
+        user.username = form_data[0]['username']
+        user.set_password(form_data[0]['password1'])
+        user.save()
+        paciente = Paciente()
+        paciente.usuario = user
+        paciente.email = form_data[0]['email']
+        paciente.nome = form_data[0]['nome']
+        paciente.nascimento = form_data[0]['nascimento']
+        paciente.sexo = form_data[0]['sexo']
+        paciente.escolaridade = form_data[0]['escolaridade']
+        paciente.nomeConjuge = form_data[1]['nomeConjuge']
+        paciente.nascimentoConjuge = form_data[1]['nascimentoConjuge']
+        paciente.sexoConjuge = form_data[1]['sexoConjuge']
+        paciente.escolaridadeConjuge = form_data[1]['escolaridadeConjuge']
+        paciente.nomePai = form_data[2]['nomePai']
+        paciente.nascimentoPai = form_data[2]['nascimentoPai']
+        paciente.falecimentoPai = form_data[2]['falecimentoPai']
+        paciente.escolaridadePai = form_data[2]['escolaridadePai']
+        paciente.nomeMae = form_data[3]['nomeMae']
+        paciente.nascimentoMae = form_data[3]['nascimentoMae']
+        paciente.falecimentoMae = form_data[3]['falecimentoMae']
+        paciente.escolaridadeMae = form_data[3]['escolaridadeMae']
+        paciente.nomeAvoPaterno = form_data[4]['nomeAvoPaterno']
+        paciente.nascimentoAvoPaterno = form_data[4]['nascimentoAvoPaterno']
+        paciente.falecimentoAvoPaterno = form_data[4]['falecimentoAvoPaterno']
+        paciente.escolaridadeAvoPaterno = form_data[4]['escolaridadeAvoPaterno']
+        paciente.nomeAvoPaterna = form_data[5]['nomeAvoPaterna']
+        paciente.nascimentoAvoPaterna = form_data[5]['nascimentoAvoPaterna']
+        paciente.falecimentoAvoPaterna = form_data[5]['falecimentoAvoPaterna']
+        paciente.escolaridadeAvoPaterna = form_data[5]['escolaridadeAvoPaterna']
+        paciente.nomeAvoMaterno = form_data[6]['nomeAvoMaterno']
+        paciente.nascimentoAvoMaterno = form_data[6]['nascimentoAvoMaterno']
+        paciente.falecimentoAvoMaterno = form_data[6]['falecimentoAvoMaterno']
+        paciente.escolaridadeAvoMaterno = form_data[6]['escolaridadeAvoMaterno']
+        paciente.nomeAvoMaterna = form_data[7]['nomeAvoMaterna']
+        paciente.nascimentoAvoMaterna = form_data[7]['nascimentoAvoMaterna']
+        paciente.falecimentoAvoMaterna = form_data[7]['falecimentoAvoMaterna']
+        paciente.escolaridadeAvoMaterna = form_data[7]['escolaridadeAvoMaterna']
+        paciente.save()
+        return render_to_response('projetofinal/cadastrado.html')
